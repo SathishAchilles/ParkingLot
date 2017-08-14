bf28e63c07212aeb73ff0dd13587f633b46824a1
Sathish <sathshh@gmail.com>
2017-08-15 05:27:08 +0530

    FEATURE: attr_accessors for vehicle attributes

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,4 +1,10 @@
 #acts as the super class for all vehicles
 class Vehicle
+  attr_accessor :registration_number, :colour
+
+  def initialize(registration_number, colour)
+    @registration_number = registration_number
+    @colour = colour
+  end
 
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -1,1 +1,17 @@
 require 'vehicle'
+
+describe Vehicle, "#registration_number" do
+  context "documenting the registration number and colour" do
+    registration_number = "KA-01-HH-1234"
+    colour = "White"
+    vehicle = Vehicle.new(registration_number, colour)
+
+    it "should accept the registration number" do
+      expect(vehicle.registration_number).to eq(registration_number)
+    end
+
+    it "should accept the colour" do
+      expect(vehicle.colour).to eq(colour)
+    end
+  end
+end
