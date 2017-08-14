38a66f2062231e9ae99a0202e5a84041ecf79d82
Sathish <sathshh@gmail.com>
2017-08-15 00:04:17 +0530

    FEATURE: new class for cars

---
+++ lib/car.rb
@@ -0,0 +1,7 @@
+require 'vehicle'
+
+#inherits from vehicle
+#default vehicle for the parking_lot
+class Car < Vehicle
+
+end
---
+++ lib/vehicle.rb
@@ -0,0 +1,4 @@
+#acts as the super class for all vehicles
+class Vehicle
+
+end
