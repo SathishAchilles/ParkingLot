686d7ed12eeeb713ca87e913b5e4d98db20d153b
Sathish <sathshh@gmail.com>
2017-08-15 03:57:26 +0530

    FIX: (ary * int) returns copies of same objects itself which is not intended..

--- lib/parking_lot.rb
+++ lib/parking_lot.rb
@@ -1,7 +1,6 @@
 require_relative 'parking_slot'
 #Implements the parking lot
 class ParkingLot
-
   def initialize
     @slots = []
   end
@@ -13,7 +12,7 @@ class ParkingLot
   def slots=(no_of_slots)
     begin
       raise ArgumentError unless no_of_slots.is_a?(Integer)
-      @slots = Array[ParkingSlot.new] * (no_of_slots || 0)
+      (no_of_slots || 0).times{ @slots << ParkingSlot.new }
     rescue ArgumentError => e
       e.backtrace
       @slots = []
