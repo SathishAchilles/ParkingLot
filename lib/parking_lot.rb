1d1dcb30d167eb8dbd202a2b68378e3f9cb2442d
Sathish <sathshh@gmail.com>
2017-08-15 01:51:26 +0530

    FEATURE: slots setup for the parking lot

--- lib/parking_lot.rb
+++ lib/parking_lot.rb
@@ -1,3 +1,25 @@
+require_relative 'parking_slot'
+#Implements the parking lot
 class ParkingLot
 
+  def initialize
+    @slots = []
+  end
+
+  def slots
+    @slots
+  end
+
+  def slots=(no_of_slots)
+    begin
+      raise ArgumentError unless no_of_slots.is_a?(Integer)
+      @slots = Array[ParkingSlot.new] * (no_of_slots || 0)
+    rescue ArgumentError => e
+      e.backtrace
+      @slots = []
+    rescue Exception => e
+      e.backtrace
+      @slots = []
+    end
+  end
 end
--- spec/parking_lot_spec.rb
+++ spec/parking_lot_spec.rb
@@ -1,1 +1,31 @@
 require 'parking_lot'
+
+describe ParkingLot, "#slots" do
+  context "hold up to 'n' cars at any given point in time" do
+    parking_lot = ParkingLot.new
+
+    it "should open up the given n slots" do
+      parking_lot.slots = 3
+      expect(parking_lot.slots.count).to eq 3
+    end
+
+    it "should be parking slots" do
+      expect(parking_lot.slots).to all(be_a(ParkingSlot))
+    end
+
+    it "should be empty when 'n' is nil" do
+      parking_lot.slots = nil
+      expect(parking_lot.slots).to be_empty
+    end
+
+    it "should be empty for any negative value for slots" do
+      parking_lot.slots = -1
+      expect(parking_lot.slots).to be_empty
+    end
+
+    it "should be empty if not a integer" do
+      parking_lot.slots = "some dummy string"
+      expect(parking_lot.slots).to be_empty
+    end
+  end
+end
