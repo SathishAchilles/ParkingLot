8612eefec210f219e278126a382e9352942afb52
Sathish <sathshh@gmail.com>
2017-08-15 05:11:03 +0530

    FEATURE: slots with increasing number from entry point

--- lib/parking_slot.rb
+++ lib/parking_slot.rb
@@ -1,4 +1,19 @@
+require_relative 'parking_layout'
+
 #class for the individual parking slots present within the parking lot
 class ParkingSlot
+  @@distance_from_the_entry_point = ParkingLayout::ENTRY_POINT
 
+  def initialize
+    set_number
+  end
+
+  def number
+    @number
+  end
+
+  protected
+  def set_number
+    @number = (@@distance_from_the_entry_point += ParkingLayout::INCREASING_STEP)
+  end
 end
--- spec/parking_slot_spec.rb
+++ spec/parking_slot_spec.rb
@@ -1,1 +1,23 @@
 require 'parking_slot'
+require 'parking_layout'
+
+describe ParkingSlot, "#number" do
+  parking_slot = ParkingSlot.new
+  context "slot is given a number" do
+    it "should start at 1 from the increasing with entry point" do
+      expect(parking_slot.number).to eq (ParkingLayout::ENTRY_POINT + 1)
+    end
+  end
+
+  context "Each slot number" do
+    slots = 3
+    parking_slots = []
+    slots.times{ parking_slots << ParkingSlot.new }
+    entry_point = parking_slot.number
+    parking_slots.each.with_index(entry_point) do |slot, k|
+      it "should increase with increasing distance: #{k} from the entry point: #{entry_point} in steps of #{ParkingLayout::INCREASING_STEP}" do
+        expect(slot.number).to eq ( entry_point += ParkingLayout::INCREASING_STEP)
+      end
+    end
+  end
+end
