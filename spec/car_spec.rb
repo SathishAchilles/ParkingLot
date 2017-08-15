8bcbb8a2e848b2f123e6e3a6f6e1dc93054b719e
Sathish <sathshh@gmail.com>
2017-08-15 05:31:58 +0530

    FEATURE: car extends vehicle and succeeds in all tests

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -1,1 +1,17 @@
 require 'car'
+
+describe Car, "#registration_number, #colour" do
+  context "documenting the registration number and colour" do
+    registration_number = "KA-01-HH-1234"
+    colour = "White"
+    vehicle = Car.new(registration_number, colour)
+
+    it "should accept the registration number" do
+      expect(vehicle.registration_number).to eq(registration_number)
+    end
+
+    it "should accept the colour" do
+      expect(vehicle.colour).to eq(colour)
+    end
+  end
+end
